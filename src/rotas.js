fab9881ac82f32fa22a6143aaa60f853a4fdbc8f
davi <daviforever17@gmail.com>
2024-09-20 11:24:09 -0300

    tarefConc

--- src/rotas.js
+++ src/rotas.js
@@ -1,7 +1,9 @@
-import musicaController from './controller/musicaController.js'
+import musicaController from './controller/musicaController.js';
+import tarefaController from './controller/tarefaController.js';
 
 export default function adicionarRotas(servidor) {
     
-    servidor.use(musicaController)
+    servidor.use(musicaController);
+    servidor.use(tarefaController);
 
 }
